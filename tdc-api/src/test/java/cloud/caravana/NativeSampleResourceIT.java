886f11cb5cb3de4422339b15b9f31c3e421e760e
EC2 Default User <ec2-user@ip-172-31-13-7.us-west-2.compute.internal>
2021-03-04 13:54:04 +0000

    boot dos projetos

---
+++ tdc-api/src/test/java/cloud/caravana/NativeSampleResourceIT.java
@@ -0,0 +1,9 @@
+package cloud.caravana;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeSampleResourceIT extends SampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}

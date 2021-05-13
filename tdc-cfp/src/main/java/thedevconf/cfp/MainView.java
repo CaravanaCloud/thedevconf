24652b77db0d3406b81f423898f0eb469e39e81f
Julio Faerman <jfaerman@gmail.com>
2021-05-13 14:08:41 +0200

    CFP update

---
+++ tdc-cfp/src/main/java/thedevconf/cfp/MainView.java
@@ -0,0 +1,20 @@
+package thedevconf.cfp;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.PWA;
+
+
+@Route("")
+@PWA(name = "TheDevConf CFP", shortName = "tdc-cfp", enableInstallPrompt = false)
+@CssImport("./styles/shared-styles.css")
+@CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
+public class MainView extends VerticalLayout {
+    public MainView(){
+        Button button = new Button("TheDevConf CFP");
+        add(button);
+    }
+}

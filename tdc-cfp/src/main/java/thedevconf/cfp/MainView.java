83e9943b883a2c03154ac9d5bdc423341dae79b1
Julio Faerman <jfaerman@gmail.com>
2021-05-17 20:22:47 +0200

    Initial navigation

--- tdc-cfp/src/main/java/thedevconf/cfp/MainView.java
+++ tdc-cfp/src/main/java/thedevconf/cfp/MainView.java
@@ -4,10 +4,12 @@ import cloud.caravana.vo.Alternatives;
 import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouterLink;
 import com.vaadin.flow.server.PWA;
 
 import javax.ws.rs.client.Client;
@@ -22,33 +24,50 @@ import java.util.Date;
 @CssImport("./styles/shared-styles.css")
 @CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
 public class MainView extends VerticalLayout {
+    String VERSION = "#7";
     public MainView(){
-        Button button = new Button("TheDevConf CFP");
+        Button button = new Button("TheDevConf CFP "+VERSION);
         button.addClickListener(e -> onClick(e));
         add(button);
+
+        Div menu = new Div();
+        menu.add(new RouterLink("home", HomeView.class));
+        add(menu);
     }
 
     private Notification onClick(ClickEvent<Button> e) {
-        String result = "???";
+        String result = "";
+        String resURL = getURL("/api/mode");
         try {
             Client client = ClientBuilder.newBuilder().build();
-            String apiURL = getApiURL();
-            WebTarget target = client.target(apiURL);
+            WebTarget target = client.target(resURL);
             Response response = target.request().get();
-            Alternatives alts = response.readEntity(Alternatives.class);
-            System.out.println(alts.toString());
+            String ctype = response.getHeaderString("content-type");
+            if (200 == response.getStatus()
+            && "application/json".equals(ctype)) {
+                Alternatives alts = response.readEntity(Alternatives.class);
+                result += alts.toString();
+            }else {
+                String body = response.readEntity(String.class);
+                result += body.toString();
+            }
             response.close();
-            result = alts.toString();
+
         }catch (Exception ex){
-            result = ex.getMessage();
+            result += ex.getMessage();
         }
+        System.out.println(result);
         return Notification.show(result);
+    }
+
+    private String getURL(String path) {
+        return getApiURL() + path;
     }
 
     private String getApiURL() {
         String apiURL = System.getenv().get("TDC_API_URL");
         if(apiURL == null){
-            apiURL = "http://localhost:8181/api/mode";
+            apiURL = "http://localhost:8181";
         }
         return apiURL;
     }

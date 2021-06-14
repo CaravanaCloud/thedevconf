b2895bd4cc557b067c5a7863d91e6057c8372d9a
Julio Faerman <jfaerman@gmail.com>
2021-06-14 21:16:26 +0200

    Initial Swagger

--- tdc-api/src/main/java/thedevconf/jaxrs/JAXRSApplication.java
+++ tdc-api/src/main/java/thedevconf/jaxrs/JAXRSApplication.java
@@ -1,8 +1,17 @@
 package thedevconf.jaxrs;
 
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+
+@OpenAPIDefinition(
+        info = @Info(
+                title="TDC API",
+                version = "0.0.1")
+)
 @ApplicationPath("")
 public class JAXRSApplication extends Application {
 

af511470a9d173fafa5f804060b37c175df8ab16
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-06-18 17:17:53 +0200

    redirecting to user area after sign in action

--- tdc-api/src/main/resources/META-INF/resources/index.js
+++ tdc-api/src/main/resources/META-INF/resources/index.js
@@ -6,6 +6,12 @@ function onSignIn(googleUser) {
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.\
 }
+function signIn() {
+    const auth2 = gapi.auth2.getAuthInstance();
+    auth2.signIn().then(function () {
+        window.location = "/user/user-area";
+    });
+}
 
 function signOut() {
     const auth2 = gapi.auth2.getAuthInstance();

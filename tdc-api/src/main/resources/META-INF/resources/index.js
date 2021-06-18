28d4dc78d5549e026efbd64ff8ddb7c1c14b2e9a
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-06-18 19:29:27 +0200

    adding function to change value of html elements

--- tdc-api/src/main/resources/META-INF/resources/index.js
+++ tdc-api/src/main/resources/META-INF/resources/index.js
@@ -1,16 +1,21 @@
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.\
+function addHTML(id, value) {
+    document.getElementById(id).innerHTML = value;
 }
+
 function signIn() {
     const auth2 = gapi.auth2.getAuthInstance();
     auth2.signIn().then(function () {
         window.location = "/user/user-area";
     });
+}
+
+function onSignIn(googleUser) {
+    const profile = googleUser.getBasicProfile();
+
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.\
 }
 
 function signOut() {

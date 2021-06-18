e6b098787d382943b402cb99d16fdbf66310f69e
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-06-18 16:32:28 +0200

    sign out for login with google Removing style of sign out google button; Redirecting to home after a sign out action;

--- tdc-api/src/main/resources/META-INF/resources/index.js
+++ tdc-api/src/main/resources/META-INF/resources/index.js
@@ -5,3 +5,10 @@ function onSignIn(googleUser) {
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
+
+function signOut() {
+    const auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}

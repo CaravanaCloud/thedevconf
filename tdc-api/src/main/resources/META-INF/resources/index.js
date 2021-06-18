e2f8dc6974ce8c6c88b8e08ba9a950c2a97b6c21
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-06-18 17:14:45 +0200

    adjusting redirect to home after sign out action

--- tdc-api/src/main/resources/META-INF/resources/index.js
+++ tdc-api/src/main/resources/META-INF/resources/index.js
@@ -1,14 +1,15 @@
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
+
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.\
 }
 
 function signOut() {
     const auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-        console.log('User signed out.');
+        window.location = "/";
     });
 }

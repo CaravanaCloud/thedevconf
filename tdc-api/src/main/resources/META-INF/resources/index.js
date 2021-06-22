da63f36ba03031cc7797d2e2f222e3d6b2894afc
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-06-22 14:55:37 +0200

    show profile with user name

--- tdc-api/src/main/resources/META-INF/resources/index.js
+++ tdc-api/src/main/resources/META-INF/resources/index.js
@@ -6,7 +6,17 @@ function signIn() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signIn().then(function () {
         console.log('User signed in.');
+        window.location="/user/user-area";
     });
+}
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.\
 }
 
 function setupLoginListeners() {
@@ -17,8 +27,6 @@ function setupLoginListeners() {
 
     auth2.currentUser.listen(userChanged);
 
-    // Start with the current live values.
-    // refreshValues();
     updateUser();
 }
 
@@ -31,20 +39,28 @@ function loadAuth2() {
 
 function signinChanged(isSignedIn) {
     console.log('signed in', isSignedIn);
+    updateUser();
 }
 
 function userChanged(googleUser) {
     console.log('user', googleUser);
+    updateUser();
 }
 
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.\
-}
+function updateUser() {
+    if (gapi.auth2){
+        const isSignedIn = gapi.auth2.getAuthInstance().isSignedIn.get();
+        if (isSignedIn) {
+            const userName = gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile().getName();
+            document.getElementById("login-header-tab").hidden = true;
+            addHTML("profile-header-tab", userName);
+            addHTML("user-name-profile", userName);
+        } else {
+            document.getElementById("profile-header-tab").hidden = false;
+            addHTML("login-header-tab", "Login");
+        }
+    }
+};
 
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();

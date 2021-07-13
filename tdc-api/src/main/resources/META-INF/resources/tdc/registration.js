d42b425e823ecf3409e3fd97f330de39b3cab469
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-07-13 17:38:09 +0200

    fixing variable declaration

--- tdc-api/src/main/resources/META-INF/resources/tdc/registration.js
+++ tdc-api/src/main/resources/META-INF/resources/tdc/registration.js
@@ -1,21 +1,22 @@
 console.log("Sample Registration Page");
-const vo = null;
+let vo = null;
 
 function postRegistrationForm() {
     const params = {"clientId":vo.clientId};
     const url = '/api/user/registration?' + new URLSearchParams(params).toString();
 
     vo.name = document.getElementById("vo_name").value;
-    vo.language = document.getElementById("vo_language").value;
+    // vo.language = document.getElementById("vo_language").value;
     vo.email = document.getElementById("vo_email").value;
-    vo.phone = document.getElementById("vo_phone").value;
-    vo.company = document.getElementById("vo_company").value;
-    vo.occupation = document.getElementById("vo_occupation").value;
-    vo.country = document.getElementById("vo_country").value;
-    vo.city = document.getElementById("vo_city").value;
-    vo.gender = document.getElementById("vo_gender").value;
-    vo.ethnicity = document.getElementById("vo_ethnicity").value;
-    vo.pcd = document.getElementById("vo_pcd").value;
+    vo.password = document.getElementById("vo_password").value;
+    // vo.phone = document.getElementById("vo_phone").value;
+    // vo.company = document.getElementById("vo_company").value;
+    // vo.occupation = document.getElementById("vo_occupation").value;
+    // vo.country = document.getElementById("vo_country").value;
+    // vo.city = document.getElementById("vo_city").value;
+    // vo.gender = document.getElementById("vo_gender").value;
+    // vo.ethnicity = document.getElementById("vo_ethnicity").value;
+    // vo.pcd = document.getElementById("vo_pcd").value;
     vo.acceptedTerms = Boolean(document.getElementById("vo_accepted_terms").value);
 
     fetch(url, {

326732602d4816eab972e12ecd3a1b25de200fd3
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-07-16 19:09:18 +0200

    adding language options to form user data

--- tdc-api/src/main/resources/META-INF/resources/tdc/formUserData.js
+++ tdc-api/src/main/resources/META-INF/resources/tdc/formUserData.js
@@ -1,16 +1,41 @@
 const url ="https://restcountries.eu/rest/v2/all";
 
 async function getCountriesData(url) {
-        const response = await fetch(url);
-        const data = await response.json();
-        if(data) {
-            showCountriesNames(data);
-        }
-        return data;
+    const response = await fetch(url);
+    const data = await response.json();
+    if(data) {
+        showCountriesNamesOptions(data);
+    }
+    return data;
 }
 
 getCountriesData(url);
 
-function showCountriesNames(data) {
-    data.forEach(data => $("#vo_country").append(`<option value="${data.translations['br']}">${data.translations['br']}</option>`));
+function showCountriesNamesOptions(data) {
+    data.forEach(data => {
+        if(data.translations['br'] === "Brasil") {
+            $("#vo_country").append(`<option selected value="${data.translations['br']}">${data.translations['br']}</option>`);
+        } else {
+            $("#vo_country").append(`<option value="${data.translations['br']}">${data.translations['br']}</option>`);
+
+        }
+    });
 }
+
+function showLanguagesOptions() {
+    const languages = {
+        PT: "Português-BR",
+        EN: "Inglês",
+        ES: "Espanhol"
+    };
+
+    Object.values(languages).forEach(language => {
+        if (language === "Português-BR") {
+            $("#vo_language").append(`<option selected value="${language}">${language}</option>`);
+        } else {
+            $("#vo_language").append(`<option value ="${language}">${language}</option>`);
+        }
+    });
+}
+
+showLanguagesOptions();

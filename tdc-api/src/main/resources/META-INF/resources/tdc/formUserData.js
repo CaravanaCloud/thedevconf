0c372abc9e2f852f275c561a0b87dc017bda133e
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-07-19 13:12:30 +0200

    adding gender options to user data form given TDC client needs

--- tdc-api/src/main/resources/META-INF/resources/tdc/formUserData.js
+++ tdc-api/src/main/resources/META-INF/resources/tdc/formUserData.js
@@ -4,38 +4,48 @@ async function getCountriesData(url) {
     const response = await fetch(url);
     const data = await response.json();
     if(data) {
-        showCountriesNamesOptions(data);
+        showCountriesOptions(data);
     }
     return data;
 }
 
-getCountriesData(url);
-
-function showCountriesNamesOptions(data) {
+function showCountriesOptions(data) {
     data.forEach(data => {
         if(data.translations['br'] === "Brasil") {
             $("#vo_country").append(`<option selected value="${data.translations['br']}">${data.translations['br']}</option>`);
-        } else {
-            $("#vo_country").append(`<option value="${data.translations['br']}">${data.translations['br']}</option>`);
-
         }
+        $("#vo_country").append(`<option value="${data.translations['br']}">${data.translations['br']}</option>`);
     });
 }
 
-function showLanguagesOptions() {
-    const languages = {
+function showLanguageOptions() {
+    const languageOptions = {
         PT: "Português-BR",
         EN: "Inglês",
         ES: "Espanhol"
-    };
+    }
 
-    Object.values(languages).forEach(language => {
+    Object.values(languageOptions).forEach(language => {
         if (language === "Português-BR") {
             $("#vo_language").append(`<option selected value="${language}">${language}</option>`);
-        } else {
-            $("#vo_language").append(`<option value ="${language}">${language}</option>`);
         }
+        $("#vo_language").append(`<option value ="${language}">${language}</option>`);
     });
 }
 
-showLanguagesOptions();
+function showGenderOptions() {
+    const genderOptions = {
+        P: "Prefiro não responder"
+    }
+
+    Object.values(genderOptions).forEach(gender => {
+        if(gender === "Prefiro não responder") {
+            $("#vo_gender").append(`<option selected value="${gender}">${gender}</option>`);
+        }
+        $("#vo_gender").append(`<option value="${gender}">${gender}</option>`);
+    });
+}
+
+getCountriesData(url);
+showLanguageOptions();
+showGenderOptions()

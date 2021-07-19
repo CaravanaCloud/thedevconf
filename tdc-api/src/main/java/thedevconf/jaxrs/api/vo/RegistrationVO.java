6d16f1121d0fe70b9c49dcfb3b88c66ed0e07864
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-07-19 17:35:35 +0200

    removing unnecessary code

--- tdc-api/src/main/java/thedevconf/jaxrs/api/vo/RegistrationVO.java
+++ tdc-api/src/main/java/thedevconf/jaxrs/api/vo/RegistrationVO.java
@@ -6,17 +6,8 @@ public class RegistrationVO {
     String userId;
     String clientId;
     String name;
-//    String language;
     String email;
     String password;
-//    String phone;
-//    String company;
-//    String occupation;
-//    String country;
-//    String city;
-//    String gender;
-//    String ethnicity;
-//    String pcd;
     Boolean acceptedTerms;
     LocalDateTime createTime;
 
@@ -25,17 +16,8 @@ public class RegistrationVO {
         this.userId = userId;
         this.clientId = clientId;
         this.name = name;
-//        this.language = language;
         this.email = email;
         this.password = password;
-//        this.phone = phone;
-//        this.company = company;
-//        this.occupation = occupation;
-//        this.country = country;
-//        this.city = city;
-//        this.gender = gender;
-//        this.ethnicity = ethnicity;
-//        this.pcd = pcd;
         this.acceptedTerms = acceptedTerms;
         this.createTime = createTime;
     }
@@ -48,25 +30,7 @@ public class RegistrationVO {
 
     public String getPassword() { return password; }
 
-//    public String getLanguage() { return language; }
-
     public String getEmail() { return email; }
-
-//    public String getPhone() { return phone; }
-//
-//    public String getCompany() { return company; }
-//
-//    public String getOccupation() { return occupation; }
-//
-//    public String getCountry() { return country; }
-//
-//    public String getCity() {  return city; }
-//
-//    public String getGender() { return gender; }
-//
-//    public String getEthnicity() { return ethnicity; }
-//
-//    public String getPcd() { return pcd; }
 
     public Boolean getAcceptedTerms() { return acceptedTerms; }
 

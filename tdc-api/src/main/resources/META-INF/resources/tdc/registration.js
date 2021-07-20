5c50c8d03d095a7316da091a16feaef4eb685ad4
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-07-20 16:14:22 +0200

    removing unnecessary code

--- tdc-api/src/main/resources/META-INF/resources/tdc/registration.js
+++ tdc-api/src/main/resources/META-INF/resources/tdc/registration.js
@@ -6,17 +6,8 @@ function postRegistrationForm() {
     const url = '/api/user/registration?' + new URLSearchParams(params).toString();
 
     vo.name = document.getElementById("vo_name").value;
-    // vo.language = document.getElementById("vo_language").value;
     vo.email = document.getElementById("vo_email").value;
     vo.password = document.getElementById("vo_password").value;
-    // vo.phone = document.getElementById("vo_phone").value;
-    // vo.company = document.getElementById("vo_company").value;
-    // vo.occupation = document.getElementById("vo_occupation").value;
-    // vo.country = document.getElementById("vo_country").value;
-    // vo.city = document.getElementById("vo_city").value;
-    // vo.gender = document.getElementById("vo_gender").value;
-    // vo.ethnicity = document.getElementById("vo_ethnicity").value;
-    // vo.pcd = document.getElementById("vo_pcd").value;
     vo.acceptedTerms = Boolean(document.getElementById("vo_accepted_terms").value);
 
     fetch(url, {

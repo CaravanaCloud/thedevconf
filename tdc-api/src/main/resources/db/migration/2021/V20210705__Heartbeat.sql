31075ad01e664b334691c0f04cc55cc457707b77
Julio Faerman <jfaerman@gmail.com>
2021-07-20 13:39:26 +0200

    fix h2 startup queries

--- tdc-api/src/main/resources/db/migration/2021/V20210705__Heartbeat.sql
+++ tdc-api/src/main/resources/db/migration/2021/V20210705__Heartbeat.sql
@@ -1,7 +1,7 @@
-CREATE TABLE `Heartbeat` (
-   `uuid` char(36) NOT NULL,
-   `acceptTime` datetime(6) DEFAULT NULL,
-   `payload` varchar(255) DEFAULT NULL,
-   `sourceIP` varchar(255) DEFAULT NULL,
-   PRIMARY KEY (`uuid`)
-) ;
+-- CREATE TABLE `Heartbeat` (
+--   `uuid` char(36) NOT NULL,
+--   `acceptTime` datetime(6) DEFAULT NULL,
+--   `payload` varchar(255) DEFAULT NULL,
+--   `sourceIP` varchar(255) DEFAULT NULL,
+--   PRIMARY KEY (`uuid`)
+--) ;

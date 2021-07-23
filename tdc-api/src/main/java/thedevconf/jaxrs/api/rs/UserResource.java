6a046ce8237b6e660eb54641acb461a4881a33bd
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-07-23 13:43:56 +0200

    to post an user

--- tdc-api/src/main/java/thedevconf/jaxrs/api/rs/UserResource.java
+++ tdc-api/src/main/java/thedevconf/jaxrs/api/rs/UserResource.java
@@ -1,5 +1,8 @@
 package thedevconf.jaxrs.api.rs;
 
+import cloud.caravana.App;
+import thedevconf.jaxrs.api.entity.User;
+import thedevconf.jaxrs.api.services.UserService;
 import thedevconf.jaxrs.api.vo.RegistrationVO;
 import thedevconf.jaxrs.auth.UserSession;
 import thedevconf.jaxrs.ui.BaseResource;
@@ -8,15 +11,19 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.sql.DataSource;
+import javax.transaction.Transactional;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+import java.util.Set;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 /**
-* API Resource for UserInformation, under Authentication
-* */
+ * API Resource for UserInformation, under Authentication
+ */
 @ApplicationScoped
-public class UserResource extends BaseResource {
+public class UserResource {
 
     @Inject
     SummaryResource summary;
@@ -28,6 +35,9 @@ public class UserResource extends BaseResource {
     //jdbc
     @Inject
     DataSource ds;
+
+    @Inject
+    UserService userService;
 
     @Path("summary")
     public SummaryResource getSummary() {
@@ -52,6 +62,16 @@ public class UserResource extends BaseResource {
         //em.merge()...
         //ds.getConnection().createStatement()
         return vo;
+    }
+
+    @Path("user-data")
+    @POST
+    @Consumes(APPLICATION_JSON)
+    @Produces(APPLICATION_JSON)
+    public User create(User user) {
+        userService.create(user);
+        System.out.println(user);
+        return user;
     }
 
     @GET
--- tdc-api/src/test/java/thedevconf/jaxrs/api/rs/UserResourceTest.java
+++ tdc-api/src/test/java/thedevconf/jaxrs/api/rs/UserResourceTest.java
@@ -3,24 +3,35 @@ package thedevconf.jaxrs.api.rs;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
+import thedevconf.jaxrs.api.entity.AcceptedTerms;
+import thedevconf.jaxrs.api.entity.User;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
 
 @QuarkusTest
-class UserResourceTest {
+public class UserResourceTest {
+
     @Test
-    public void shouldAcceptPostsOfUserData() {
+    public void testShouldAcceptPostsOfUserData() {
+        String userData = "{\"language\":\"Português-BR\"," +
+                "\"phone\":\"123456789\"," +
+                "\"company\":\"Caravana Cloud\"," +
+                "\"occupation\":\"Desenvolvedora\"," +
+                "\"country\":\"Brasil\"," +
+                "\"city\":\"Barcelona\"," +
+                "\"gender\":\"Prefiro não responder\"," +
+                "\"ethnicity\":\"Prefiro não responder\"," +
+                "\"pcd\":\"Não se aplica\", " +
+                "\"acceptedTerms\": true}";
+
         given()
                 .when()
                 .contentType(ContentType.JSON)
-                .body("{\"language\":\"Espanhol\",\"phone\":\"+1 (979) 329-4257\",\"company\":\"Mathews and Mccarthy " +
-                        "Trading\",\"occupation\":\"Consequatur quaerat\",\"country\":\"Burkina\",\"city\":\"one\"," +
-                        "\"gender\":\"Prefiro não responder\",\"ethnicity\":\"Prefiro não responder\",\"pcd\":\"Não " +
-                        "se aplica" +
-                        "\", " +
-                        "\"acceptedTerms\": " +
-                        "true}")
+                .body(userData)
                 .post("/api/user/user-data")
                 .then().statusCode(200);
     }

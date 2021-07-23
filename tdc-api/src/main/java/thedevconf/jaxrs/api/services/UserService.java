2dd863a482a93b7124d404028b0a89ee42fe7ccd
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-07-23 13:49:32 +0200

    to create an user

---
+++ tdc-api/src/main/java/thedevconf/jaxrs/api/services/UserService.java
@@ -0,0 +1,23 @@
+package thedevconf.jaxrs.api.services;
+
+import thedevconf.jaxrs.api.entity.AcceptedTerms;
+import thedevconf.jaxrs.api.entity.User;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+@ApplicationScoped
+public class UserService {
+
+    @Inject
+    EntityManager em;
+
+    @Transactional
+    public void create(User user) {
+        System.out.println(user);
+        em.merge(user);
+    }
+
+}

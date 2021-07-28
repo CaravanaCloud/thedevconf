5afadaf669f51920e3657e5f9f8be4a50a10ca29
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-07-28 19:24:58 +0200

    adding migration table register users that want to grant basic pass

---
+++ tdc-api/src/main/resources/db/migration/2021/V20210728__RegisterToGrantBasicPass.sql
@@ -0,0 +1,8 @@
+CREATE TABLE RegisterToGrantBasicPass_tdc (
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    acceptedTerms int NOT NULL,
+    emailSendingStatus varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+);

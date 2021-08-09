ecf27f4634b1a17aabd570cbbe322bba70ecf265
Maximillian Arruda <dearrudam@gmail.com>
2021-08-09 18:31:19 -0300

    Implementado a submissão e tratamento para novos registro com nome, email e senha (#49)
    * Ignoring unnecessary files from Eclipse ans VSCode
    
    * Fix - Renomeado arquivo para evitar problemas com Flyway
    
    * Fix - Adequação dos nomes das classes e internacionalização
    
    * Adicionado funções para exibir mensagens de alertas
    
    * Adicionado página de configuração de registro
    
    * Implementado a submissão e tratamento para novo registro

--- tdc-api/src/main/resources/META-INF/resources/tdc/utils.js
+++ tdc-api/src/main/resources/META-INF/resources/tdc/utils.js
@@ -36,3 +36,24 @@ function addHTML(id, value) {
         debug(`No element found by id [${id}]`);
     }
 }
+
+function showAlertWarning(placeHolderId, message) {
+    showAlert(placeHolderId, message, 'warning');
+}
+
+function clearAlerts(placeHolderId){
+    const alertPlaceholder = document.getElementById(placeHolderId);
+    if (alertPlaceholder) {
+        alertPlaceholder.innerHTML='';
+    }
+}
+
+function showAlert(placeHolderId, message, type) {
+
+    const alertPlaceholder = document.getElementById(placeHolderId);
+    if (alertPlaceholder) {
+        var wrapper = document.createElement('div')
+        wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+        alertPlaceholder.append(wrapper)
+    }
+}

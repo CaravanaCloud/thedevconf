2564fe96bf8993c10f59c2f97ab5f061a3be9b03
Julio Faerman <jfaerman@gmail.com>
2021-08-10 10:17:13 +0200

    remove old stuff

--- tdc-cfp/src/main/java/thedevconf/jaxrs/cfp/SecurityUtils.java
+++
@@ -1,9 +0,0 @@
-package thedevconf.jaxrs.cfp;
-
-public class SecurityUtils {
-
-    public static boolean isUserLoggedIn() {
-        boolean isLoggedIn = true;
-        return isLoggedIn;
-    }
-}

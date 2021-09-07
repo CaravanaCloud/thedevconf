e697eb0736d1015810f1efa52a09c5e616b7ad82
Maximillian Arruda <dearrudam@gmail.com>
2021-09-06 22:37:35 -0300

    Removed unused imports

--- tdc-api/src/test/java/cloud/caravana/SampleResourceTest.java
+++ tdc-api/src/test/java/cloud/caravana/SampleResourceTest.java
@@ -3,9 +3,6 @@ package cloud.caravana;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
-
 @QuarkusTest
 public class SampleResourceTest {
 

3892a10e6d8e79c5af1a8f9f1ba9d4e1b517225a
Marcus Pereira <mpereirassa@gmail.com>
2021-09-08 08:59:27 -0300

    change working directory to run github action for frontend

--- frontend/tests/unit/example.spec.js
+++ frontend/tests/unit/example.spec.js
@@ -1,12 +1,11 @@
-// import { shallowMount } from '@vue/test-utils'
-// import HelloWorld from '@/components/HelloWorld.vue'
+import { shallowMount } from "@vue/test-utils";
 
-// describe('HelloWorld.vue', () => {
-//   it.skip('renders props.msg when passed', () => {
-//     const msg = 'new message'
-//     const wrapper = shallowMount(HelloWorld, {
-//       propsData: { msg }
-//     })
-//     expect(wrapper.text()).toMatch(msg)
-//   })
-// })
+describe("HelloWorld.vue", () => {
+  it.skip("renders props.msg when passed", () => {
+    const msg = "new message";
+    const wrapper = shallowMount(HelloWorld, {
+      propsData: { msg },
+    });
+    expect(wrapper.text()).toMatch(msg);
+  });
+});

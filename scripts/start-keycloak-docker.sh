efdb7cbaa2d947a909a94841ac5349daedacb236
Julio Faerman <jfaerman@gmail.com>
2022-02-14 09:36:05 +0100

    Keycloak init script

---
+++ scripts/start-keycloak-docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source "$DIR/utilities.sh"
+
+docker run -p 0.0.0.0:8080:8080 \
+    -e KEYCLOAK_ADMIN="$(get_param TDC_DEVENV_KEYCLOAK_ADMIN)" \
+    -e KEYCLOAK_ADMIN_PASSWORD="$(get_param TDC_DEVENV_KEYCLOAK_ADMIN_PASSWORD)" \
+    -e DB_ADDR="$(get_param TDC_DEVENV_DB_HOST)" \
+    -e DB_PORT="3306" \
+    -e DB_DATABASE="keycloak" \
+    -e DB_USER="$(get_param TDC_DEVENV_DB_USERNAME)" \
+    -e DB_PASSWORD="$(get_param TDC_DEVENV_DB_PASSWORD)" \
+    quay.io/keycloak/keycloak:17.0.0 \
+    start-dev
+
+# end_script
---
+++ scripts/utilities.sh
@@ -0,0 +1,13 @@
+get_param () {
+    declare -n curr_value=$1
+    if [ -z "$curr_value" ]
+    then
+        aws ssm get-parameter --name "$1" --query 'Parameter.Value' --output 'text'
+    else
+        echo "${curr_value}"
+    fi  
+}
+
+end_script () {
+    echo "done"
+}

44148529b7d65b861dd530969d6f686ebc1e86d5
Julio Faerman <jfaerman@gmail.com>
2022-02-14 10:50:25 +0100

    Keycloak init script

--- scripts/start-keycloak-devenv.sh
+++ scripts/start-keycloak-devenv.sh
@@ -6,9 +6,10 @@ source "$DIR/utilities.sh"
 docker run -ti --rm -p 0.0.0.0:8080:8080 \
     -e KEYCLOAK_ADMIN="$(get_param TDC_DEVENV_KEYCLOAK_ADMIN)" \
     -e KEYCLOAK_ADMIN_PASSWORD="$(get_param TDC_DEVENV_KEYCLOAK_ADMIN_PASSWORD)" \
+    -e DB_VENDOR="mysql"
     -e DB_ADDR="$(get_param TDC_DEVENV_DB_HOST)" \
     -e DB_PORT="3306" \
-    -e DB_DATABASE="keycloak" \
+    -e DB_DATABASE="keycloak?useSSL=false" \
     -e DB_USER="$(get_param TDC_DEVENV_DB_USERNAME)" \
     -e DB_PASSWORD="$(get_param TDC_DEVENV_DB_PASSWORD)" \
     quay.io/keycloak/keycloak:17.0.0 \

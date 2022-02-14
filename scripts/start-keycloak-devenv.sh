8bd9e5be5f0c284e0ec6354e9b9e019b4b4caf17
Julio Faerman <jfaerman@gmail.com>
2022-02-14 09:31:02 +0100

    Keycloak init script

--- scripts/start-keycloak-devenv.sh
+++ scripts/start-keycloak-devenv.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source "$DIR/utilities.sh"
 
-docker run -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=Masterkey123 jboss/keycloak
+KEYCLOAK_ADMIN=admin
+
+echo docker run -p 8080:8080 \
+    -e KEYCLOAK_ADMIN=$KEYCLOAK_ADMIN \
+    -e KEYCLOAK_ADMIN_PASSWORD="$(get_parameter KEYCLOAK_ADMIN_PASSWORD)" \
+    quay.io/keycloak/keycloak:17.0.0 \
+    start-dev
+
+end_script

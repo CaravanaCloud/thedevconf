be91c7513ee1f96aaf797bc4242848168442d837
Julio Faerman <jfaerman@gmail.com>
2022-02-14 13:29:07 +0100

    Keycloak init script

--- scripts/start-keycloak-devenv.sh
+++ scripts/start-keycloak-devenv.sh
@@ -3,20 +3,21 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$DIR/utilities.sh"
 
-KEYCLOAK_VERSION=16.1.1
+#KEYCLOAK_VERSION=17.0.0
 #KEYCLOAK_IMAGE=quay.io/keycloak/keycloak
+KEYCLOAK_VERSION=16.1.1
 KEYCLOAK_IMAGE=jboss/keycloak
-KEYCLOAK_TAG=KEYCLOAK_IMAGE:KEYCLOAK_VERSION
-
+KEYCLOAK_TAG=$KEYCLOAK_IMAGE:$KEYCLOAK_VERSION
 
 docker run -ti --rm -p 0.0.0.0:8080:8080 \
     -e KEYCLOAK_ADMIN="$(get_param TDC_DEVENV_KEYCLOAK_ADMIN)" \
     -e KEYCLOAK_ADMIN_PASSWORD="$(get_param TDC_DEVENV_KEYCLOAK_ADMIN_PASSWORD)" \
+    -e KC_HOSTNAME_STRICT="false" \
     -e DB_VENDOR="mysql" \
     -e DB_ADDR="$(get_param TDC_DEVENV_DB_HOST)" \
     -e DB_PORT="3306" \
     -e DB_DATABASE="keycloak?useSSL=false" \
     -e DB_USER="$(get_param TDC_DEVENV_DB_USERNAME)" \
     -e DB_PASSWORD="$(get_param TDC_DEVENV_DB_PASSWORD)" \
-    $KEYCLOAK_IMAGE
+    $KEYCLOAK_TAG
 # end_script
